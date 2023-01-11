d87102fcc513bd152e065cddbb95419bfb30cfa0
Yherald Bautista <YheraldBautista@users.noreply.github.com>
2023-01-11 15:39:37 +0100

    Getting valentines

--- pynder_sesion.py
+++ pynder_sesion.py
@@ -3,9 +3,9 @@ import pynder
 
 def main():
     
-    session = pynder.Session(100089023886773, 'EAANWMpZCat44BAGo4bcZAKvHOMawSxlPBa9Q1afjYIBELX62N2iFnstZC0KNaZC7YtGLupKzS3ugM2dTBmgLJbsEug63ooRGoZATKp95lgw4P6PsBdavbSp1Ymhr43TvrXW0a2E4YN4g0pmtUVfxe4EiCT8J7biZCcfH0sRq1vVZBZBsM5TvyErQi9RhMpNsuDMZD')
-    session.update_location('40.416775', '-3.703790') # updates latitude and longitude for your profile
-    session.profile  # your profile. If you update its attributes they will be updated on Tinder.
+    session = pynder.Session(FACEBOOK_ID, USER_TOKEN)
+    session.update_location('LAT', 'LON') # updates latitude and longitude for your profile
+    session.profile  # your profile. 
     users = session.nearby_users() # returns a list of users nearby
     
     
